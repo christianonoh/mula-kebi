6815ddaf5e41e5a344e9005c631211e058785767
christianonoh <onohchibyk@gmail.com>
2023-11-14 17:59:32 +0100

    Add Blog document type to contentlayer.config.js

---
+++ contentlayer.config.js
@@ -0,0 +1,53 @@
+import { makeSource, defineDocumentType } from '@contentlayer/source-files'
+
+
+const Blog = defineDocumentType(() => ({
+  name: 'Blog',
+  filePathPattern: '**/**/*.mdx',
+  fields: {
+    title: {
+      type: 'string',
+      required: true,
+    },
+    author: {
+      type: 'string',
+      required: true,
+    },
+    description: {
+      type: 'string',
+      required: true,
+    },
+    image: { type: 'string',
+    required: true, 
+    },
+    tags: {
+      type: 'list',
+      of: { type: 'string', },
+      required: true,
+    },
+    publishedAt: {
+      type: 'date',
+      required: true,
+    },
+    updatedAt: {
+      type: 'date',
+      required: true,
+    },
+    isPublished: {
+      type: 'boolean',
+      default: true,
+    },
+  },
+  computedFields: {
+    url: {
+      type: 'string',
+      resolve: (doc) => `blogs/${doc._raw.flattenedPath}`,
+    },
+  },
+}))
+
+export default makeSource({
+  /* options */
+  contentDirPath: './content',
+  documentTypes: [Blog],
+})
