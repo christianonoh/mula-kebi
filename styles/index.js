2c5926c34d5724475a0278e697a8e7bbd57c0b23
christianonoh <onohchibyk@gmail.com>
2023-11-15 11:22:00 +0100

    Add styles for inner and inter width, paddings, margins, flex, buttons, and effects.

---
+++ styles/index.js
@@ -0,0 +1,25 @@
+const styles = {
+  innerWidth: '2xl:max-w-[1280px] w-full',
+  interWidth: 'lg:w-[80%] w-[100%]',
+
+  paddings: 'sm:p-16 xs:p-8 px-6 py-12',
+  yPaddings: 'sm:py-16 xs:py-8 py-12',
+  xPaddings: 'sm:px-16 px-6',
+  topPaddings: 'sm:pt-16 xs:pt-8 pt-12',
+  bottomPaddings: 'sm:pb-16 xs:pb-8 pb-12',
+
+  xMargins: 'sm:mx-16 mx-6',
+
+  flexCenter: 'flex justify-center items-center',
+  flexStart: 'flex justify-start items-start',
+  flexEnd: 'flex justify-end',
+  navPadding: 'pt-[98px]',
+
+  // buttons
+  blackBtn: 'border border-slate-800 rounded-full bg-slate-800 py-1.5 px-5 text-primary transition-all hover:bg-white hover:text-red text-center text-sm flex items-center justify-center',
+
+  // effects
+  underline: 'bg-gradient-to-r from-accent to-accent bg-[length:0px_6px] hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500',
+};
+
+export default styles;
