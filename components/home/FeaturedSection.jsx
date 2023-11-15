c0d0f55dcdcf0da72ba2946e4d33ac0a5ffb0422
christianonoh <onohchibyk@gmail.com>
2023-11-15 20:45:04 +0100

    Add FeaturedSection component with sorted blogs and featured posts

---
+++ components/home/FeaturedSection.jsx
@@ -0,0 +1,34 @@
+import { cx, sortBlogs } from "@/utils"
+import BlogLayoutOne from "../blog/BlogLayoutOne"
+import BlogLayoutTwo from "../blog/BlogLayoutTwo"
+import { allBlogs } from "@/.contentlayer/generated"
+import styles from "@/styles"
+
+
+const FeaturedSection = () => {
+  const sortedBlogs = sortBlogs(allBlogs)
+  
+  return (
+    <section
+      // className={cx(styles.xMargins2x, styles.flexCenter, styles.topMargins, 'flex-col')}
+      className='flex flex-col items-center justify-center w-full px-5 mt-16 sm:mt-24 md:mt-32 sm:px-10 md:px-24 sxl:px-32'
+    >
+      <h2 className="w-full text-4xl font-bold capitalize">
+        featured posts
+      </h2>
+      <div className={cx(styles.topMargins, "grid grid-cols-2 grid-rows-2 gap-6")}>
+        <article className="relative col-span-2 row-span-2 lg:col-span-1 red">
+          <BlogLayoutOne blog={sortedBlogs[9]} />
+        </article>
+        <article className="relative col-span-2 row-span-1 sm:col-span-1 blue">
+          <BlogLayoutTwo blog={sortedBlogs[6]} />
+        </article>
+        <article className="relative col-span-2 row-span-1 sm:col-span-1 blue">
+          <BlogLayoutTwo blog={sortedBlogs[4]} />
+        </article>
+      </div>
+    </section>
+  )
+}
+
+export default FeaturedSection
