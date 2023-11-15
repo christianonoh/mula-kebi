5aec5e5c379c0cb4d2de2dd33a65169bbb710c8b
christianonoh <onohchibyk@gmail.com>
2023-11-15 20:46:19 +0100

    Add formatDate function to utils/index.js

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,4 @@
-import { compareDesc, parseISO } from "date-fns";
+import { compareDesc, parseISO, format } from "date-fns";
 
 export const cx = (...classNames) => classNames.filter(Boolean).join(' ');
 
@@ -6,4 +6,8 @@ export const sortBlogs = (blogs) => {
   return blogs.slice().sort((a, b) => {
     return compareDesc(parseISO(b.publishedAt), parseISO(a.publishedAt));
   });
+}
+
+export const formatDate = (date, dateFormat='MMMM dd, yyyy') => {
+  return format(parseISO(date), dateFormat);
 }
