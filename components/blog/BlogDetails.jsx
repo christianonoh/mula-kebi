576ef6a8ca215d85255951d7d2f2fa4a66599e37
christianonoh <onohchibyk@gmail.com>
2023-11-21 19:47:47 +0100

    Add BlogDetails component

---
+++ components/blog/BlogDetails.jsx
@@ -0,0 +1,15 @@
+import { formatDate } from "@/utils"
+import Link from "next/link"
+
+const BlogDetails = ({ blog, slug }) => {
+  return (
+    <div className="flex flex-wrap justify-around px-5 py-2 mx-5 mt-8 text-xl rounded-lg bg-accent text-light md:mx-10">
+      <time className='m-3'>{formatDate(blog.publishedAt)}</time>
+      <div className='m-3'>{'1200 views'}</div>
+      <div className='m-3'>{blog.readingTime.text}</div>
+      <Link href={`/categories/${blog.tags[0]}`} className='m-3'>#{blog.tags[0]}</Link>
+    </div>
+  )
+}
+
+export default BlogDetails
