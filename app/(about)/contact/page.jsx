65d1d8f3efcddb2be1e958be7eea257f93d274c9
christianonoh <onohchibyk@gmail.com>
2023-11-22 22:30:58 +0100

    Group about and contact pages inorder to use shared layout

---
+++ app/(about)/contact/page.jsx
@@ -0,0 +1,30 @@
+import ContactForm from '@/components/elements/contactForm';
+import Character from "@/public/images/character.png";
+import Image from 'next/image';
+
+const contactPage = () => {
+  return (
+    <article>
+      <section className="flex flex-col items-center justify-center border-b-2 md:flex-row border-dark">
+        <div
+          className="items-center justify-center w-full md:w-2/5 md:h-[60vh] h-[40vh] border-r-2
+ border-dark"
+        >
+          <Image
+            src={Character}
+            alt='Character happy'
+            className="object-contain h-full"
+          />
+        </div>
+        <div className="items-center justify-center w-full px-5 py-10 lg:px-16 md:w-3/5">
+          <h2 className="mb-4 text-4xl font-semibold text-center sm:text-5xl text-dark lg:text-5xl">
+          Let's Connect!
+          </h2>
+          <ContactForm />
+        </div>
+      </section>
+    </article>
+  )
+}
+
+export default contactPage
