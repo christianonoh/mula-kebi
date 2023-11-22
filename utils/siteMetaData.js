b41bc436416cc69fa31acb2de30579738d18299c
christianonoh <onohchibyk@gmail.com>
2023-11-22 22:31:27 +0100

    Add new images and update site metadata

---
+++ utils/siteMetaData.js
@@ -0,0 +1,20 @@
+// please update required information
+const siteMetadata = {
+  title: 'Mula Kebi Blog',
+  author: 'Christian Onoh',
+  headerTitle: 'Mula Blog',
+  description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+  language: 'en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://mulakebi.vercel.app', // your website URL
+  siteLogo: '/images/logo.svg',
+  socialBanner: '/social-banner.png', // add social banner in the public folder
+  email: 'chibyk5000@gmail.com', 
+  github: 'https://github.com/christianonoh',
+  twitter: 'https://twitter.com/christianonoh',
+  facebook: 'https://facebook.com',
+  linkedin: 'https://www.linkedin.com/in/christianonoh/',
+  locale: 'en-US',
+}
+
+module.exports = siteMetadata
