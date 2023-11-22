7bd311678c1f5307dc3db4f280f45808a1b11d1d
christianonoh <onohchibyk@gmail.com>
2023-11-22 14:01:54 +0100

    Add animation styles for marquee and marquee2

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,20 @@ module.exports = {
         manrope: ['var(--font-manrope)'],
         inter: ['var(--font-inter)'],
       },
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+},
     },
   },
   plugins: [
