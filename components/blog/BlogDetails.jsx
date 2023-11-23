43668c4a7e572e07cfe565cf06424014e97a251d
christianonoh <onohchibyk@gmail.com>
2023-11-23 03:44:16 +0100

    Fix slug import and add ViewCounter component

--- components/blog/BlogDetails.jsx
+++ components/blog/BlogDetails.jsx
@@ -1,14 +1,15 @@
 import { formatDate } from "@/utils"
 import Link from "next/link"
-import { slug } from 'github-slugger';
+import { slug as slugger } from 'github-slugger';
+import ViewCounter from "./ViewCounter";
 
-const BlogDetails = ({ blog }) => {
+const BlogDetails = ({ blog, slug }) => {
   return (
     <div className="flex flex-wrap justify-around px-5 py-2 mx-5 mt-8 text-xl rounded-lg bg-accent text-light md:mx-10">
       <time className='m-3'>{formatDate(blog.publishedAt)}</time>
-      <div className='m-3'>{'1200 views'}</div>
+      <div className='m-3'><ViewCounter slug={slug} /></div>
       <div className='m-3'>{blog.readingTime.text}</div>
-      <Link href={`/categories/${slug(blog.tags[0])}`} className='m-3'>#{blog.tags[0]}</Link>
+      <Link href={`/categories/${slugger(blog.tags[0])}`} className='m-3'>#{blog.tags[0]}</Link>
     </div>
   )
 }
