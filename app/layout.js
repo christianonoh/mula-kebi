e644ee7649ef3c62d0b605145958318ad903efed
christianonoh <onohchibyk@gmail.com>
2023-11-23 13:11:26 +0100

    Add site metadata to layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import '@/styles/globals.css'
 import Header from '@/components/header'
 import { cx } from '@/utils'
 import Footer from '@/components/footer'
+import siteMetadata from '@/utils/siteMetaData'
 
 const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-inter'})
 
@@ -10,8 +11,51 @@ const manrope = Manrope({ subsets: ['latin'], display: 'swap', variable: '--font
 
 
 export const metadata = {
-  title: 'Mula Blog',
-  description: 'A simple blog built with Next.js and Tailwind CSS.',
+  metadataBase: new URL(siteMetadata.siteUrl),
+  title: {
+    template: `%s | ${siteMetadata.title}`,
+    default: siteMetadata.title, // a default is required when creating a template
+  },
+  description: siteMetadata.description,
+  openGraph: {
+    title: siteMetadata.title,
+    description: siteMetadata.description,
+    url: siteMetadata.siteUrl,
+    siteName: siteMetadata.title,
+    images: [
+      {
+        url: siteMetadata.socialBanner,
+        width: 800,
+        height: 600,
+      },
+      {
+        url: siteMetadata.socialBanner,
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteMetadata.title,
+    images: [siteMetadata.socialBanner],
+  },
 }
 
 export default function RootLayout({ children }) {
