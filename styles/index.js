9866afea71ae5eb6e9cf40554ec6037cf7fb3db2
Christian Onoh <chibyk5000@gmail.com>
2023-12-09 12:44:30 +0100

    Update styles padding and margins

--- styles/index.js
+++ styles/index.js
@@ -2,7 +2,7 @@ const styles = {
   innerWidth: '2xl:max-w-[1280px] w-full',
   interWidth: 'lg:w-[80%] w-[100%]',
 
-  paddings: 'sm:p-16 xs:p-8 px-6 py-12',
+  paddings: 'lg:p-16 md:p-12 sm:p-8 p-6',
   yPaddings: 'sm:py-16 xs:py-8 py-12',
   xPaddings: 'sm:px-16 px-6',
   topPaddings: 'sm:pt-16 xs:pt-8 pt-12',
@@ -12,7 +12,7 @@ const styles = {
   xMargins: 'sm:mx-16 mx-6',
   xMargins2x: 'sm:mx-32 mx-6',
   yMargins: 'sm:my-16 xs:my-8 my-12',
-  topMargins: 'sm:mt-16 xs:mt-8 mt-12',
+  topMargins: 'md:mt-16 mt-10',
   bottomMargins: 'sm:mb-16 xs:mb-8 mb-12',
 
   flexCenter: 'flex justify-center items-center',
@@ -24,7 +24,7 @@ const styles = {
   blackBtn: 'border border-slate-800 rounded-full bg-slate-800 py-1.5 px-5 text-primary transition-all hover:bg-white hover:text-red text-center text-sm flex items-center justify-center',
 
   // effects
-  underline: 'bg-gradient-to-r from-accent to-accent bg-[length:0px_6px] hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500',
+  underline: 'bg-gradient-to-r from-accent dark:from-accentDark/50 dark:to-accentDark/50 to-accent bg-[length:0px_6px] hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500',
 };
 
 export default styles;
