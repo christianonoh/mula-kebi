2640fe1ba29eb5dabb1c3d7a08ff26d378930a5b
Christian Onoh <chibyk5000@gmail.com>
2023-12-09 12:43:52 +0100

    Refactor RecentPosts component for dark mode

--- components/home/RecentPosts.jsx
+++ components/home/RecentPosts.jsx
@@ -1,6 +1,4 @@
 import { cx, sortBlogs } from "@/utils"
-import BlogLayoutOne from "../blog/BlogLayoutOne"
-import BlogLayoutTwo from "../blog/BlogLayoutTwo"
 import { allBlogs } from "@/.contentlayer/generated"
 import styles from "@/styles"
 import BlogLayoutThree from "../blog/BlogLayoutThree"
@@ -17,10 +15,10 @@ const RecentPosts = () => {
       className='flex flex-col items-center justify-center w-full px-5 mt-16 sm:mt-24 md:mt-32 sm:px-10 md:px-24 sxl:px-32'
     >
       <div className="flex items-center justify-between w-full ">
-        <h2 className="text-4xl font-bold capitalize ">
+        <h2 className="w-full text-2xl font-bold capitalize md:text-4xl dark:text-light">
           recent posts
         </h2>
-        <Link href='/catergories/all' className="px-4 py-2 capitalize rounded-full hover:text-light text-accent bg-accent/20">
+        <Link href='/catergories/all' className="px-4 py-2 text-sm font-semibold capitalize rounded-full sm:text-base md:text-lg whitespace-nowrap hover:text-dark text-accent bg-accent/10 dark:hover:text-light dark:text-accentDark dark:bg-accentDark/10">
           view all
         </Link>
       </div>
