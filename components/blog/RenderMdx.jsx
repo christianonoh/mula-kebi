552ef553460d6d4192170487e72b7575e2dd7bf7
Christian Onoh <chibyk5000@gmail.com>
2023-12-09 12:42:33 +0100

    Refactor RenderMdx component for dark mode

--- components/blog/RenderMdx.jsx
+++ components/blog/RenderMdx.jsx
@@ -1,30 +1,37 @@
-"use client"
+"use client";
 
-import { useMDXComponent } from 'next-contentlayer/hooks'
-import Image from 'next/image'
+import { useMDXComponent } from "next-contentlayer/hooks";
+import Image from "next/image";
 
 const RenderMdx = ({ blog }) => {
   // Parse the MDX file via the useMDXComponent hook.
-  const Content = useMDXComponent(blog.body.code)
+  const Content = useMDXComponent(blog.body.code);
 
   const blogComponents = {
     Image,
-  }
+  };
   return (
-    <article className='col-span-8 prose prose-lg font-inter max-w-max
-      prose-blockquote:py-2
-      prose-blockquote:px-6
-      prose-blockquote:rounded-r-lg
-      prose-blockquote:not-italic
+    <div className='col-span-12  lg:col-span-8 font-in prose sm:prose-base md:prose-lg max-w-max
+    prose-blockquote:bg-accent/20 
+    prose-blockquote:p-2
+    prose-blockquote:px-6
     prose-blockquote:border-accent
-    prose-blockquote:bg-accent/20
-    
-    prose-li:marker:text-accent
-    
-    '>
-      <Content components={blogComponents} />
-    </article>
-  )
-}
+    prose-blockquote:not-italic
+    prose-blockquote:rounded-r-lg
 
-export default RenderMdx
+    prose-li:marker:text-accent
+
+    dark:prose-invert
+    dark:prose-blockquote:border-accentDark
+    dark:prose-blockquote:bg-accentDark/20
+    dark:prose-li:marker:text-accentDark
+
+    first-letter:text-3xl
+    sm:first-letter:text-5xl
+    
+    '>   <Content components={blogComponents} />
+    </div>
+  );
+};
+
+export default RenderMdx;
