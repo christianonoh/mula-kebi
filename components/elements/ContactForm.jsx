66eee47d1e9ea052a45491aeb9b5fa591e2230e7
Christian Onoh <chibyk5000@gmail.com>
2023-12-09 12:42:40 +0100

    Update ContactForm styles for dark mode

--- components/elements/ContactForm.jsx
+++ components/elements/ContactForm.jsx
@@ -5,12 +5,12 @@ import { useForm } from 'react-hook-form';
 const ContactForm = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = data => console.log(data);
-  
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="inline-block text-lg font-semibold">
+      <div className="inline-block text-base sm:text-lg font-medium md:font-semibold text-dark dark:text-light">
         <span>Hello! My name is </span>
-        <input type="text" placeholder="name" {...register("name", {required: true, min: 5, maxLength: 30})} className='p-0 mx-2 bg-transparent border-0 border-b outline-none focus:ring-0 placeholder:text-center placeholder:text-lg border-gray focus:border-gray' />
+        <input type="text" placeholder="name" {...register("name", {required: true, min: 5, maxLength: 30})} className='p-0 mx-2 bg-transparent border-0 border-b outline-none focus:ring-0 placeholder:text-center placeholder:text-lg border-dark/50 dark:border-light/50 focus:border-gray' />
         <span>and I want to discuss a potential project. You can email me at </span>
         <input type="text" placeholder="email" {...register("email", {required: true})} className='p-0 mx-2 bg-transparent border-0 border-b outline-none focus:ring-0 placeholder:text-center placeholder:text-lg border-gray focus:border-gray' />
         <span>or reach out to me on </span>
@@ -19,7 +19,7 @@ const ContactForm = () => {
         <textarea {...register("message", {required: true, max: 5, min: 5, maxLength: 50})} className='w-full p-0 mx-0 bg-transparent border-0 border-b outline-none focus:ring-0 placeholder:text-lg border-gray focus:border-gray' />
       </div>
 
-      <input type="submit" className="inline-block px-6 py-2 mt-8 text-lg font-medium capitalize border-2 border-solid rounded cursor-pointer sm:text-xl sm:py-3 sm:px-8 border-dark" />
+      <input type='submit' className="inline-block px-6 py-2 mt-8 text-lg font-medium capitalize border-2 border-solid rounded cursor-pointer sm:text-xl sm:py-3 sm:px-8 border-dark dark:border-light dark:text-light" />
     </form>
   )
 }
