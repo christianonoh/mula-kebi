14f2227d43ac7c870eb895be3feaa54edad9381d
Christian Onoh <chibyk5000@gmail.com>
2023-12-09 12:43:13 +0100

    Update footer styles and add dark mode support

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -12,26 +12,26 @@ const Footer = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = data => console.log(data);
   return (
-    <footer className={cx(styles.yMargins, styles.yPaddings, "rounded-3xl mx-5 sm:mx-10 bg-dark text-light")}>
+    <footer className={cx(styles.yMargins, styles.yPaddings, "rounded-3xl mx-5 sm:mx-10 bg-dark dark:bg-accentDark/90 text-light dark:text-dark dark:font-medium")}>
       <div className={cx(styles.flexCenter, styles.xPaddings, "flex-col ")}>
-        <h2 className="text-3xl text-center">
+        <h2 className="text-3xl font-medium text-center dark:font-bold">
         Interesting Stories | Updates | Guides
         </h2>
         <p className="max-w-sm my-5 text-center font-inter">
           Subscribe to learn about new technology and updates. Join over 5000+ members community to stay up to date with latest news.
         </p>
         <form
-          className="flex items-stretch xs:min-w-[384px] p-2 my-6 rounded-md bg-light"
+          className="flex items-stretch xs:min-w-[384px] p-2 my-6 rounded-md bg-light dark:bg-dark"
           onSubmit={handleSubmit(onSubmit)}>
           <input type="email" 
-            className="py-2 bg-transparent border-0 border-b border-solid focus:border-dark focus:ring-0 text-dark font-inter" 
+            className="py-2 bg-transparent border-0 border-b border-solid focus:border-dark focus:ring-0 text-dark dark:text-light font-inter" 
             placeholder="Enter your email"
             {...register("Email", {required: true, pattern: /^\S+@\S+$/i})}
           />
-          <input type="submit" className="px-4 py-2 ml-2 rounded-lg flex-0 bg-dark text-light" />
+          <input type="submit" className="px-4 py-2 ml-2 rounded-md flex-0 bg-dark dark:bg-light text-light dark:text-dark dark:font-semibold" />
         </form>
         <div className='flex items-center gap-2'>
-          <a href={siteMetadata.github} className='w-6 h-6'><GithubIcon className='transition-all duration-200 ease-in-out fill-light hover:scale-125' /></a>
+          <a href={siteMetadata.github} className='w-6 h-6'><GithubIcon className='transition-all duration-200 ease-in-out fill-light dark:fill-dark hover:scale-125' /></a>
           <a href={siteMetadata.twitter} className='w-6 h-6'><TwitterIcon className='transition-all duration-200 ease-in-out hover:scale-125' /></a>
           <a href={siteMetadata.linkedin} className='w-6 h-6'><LinkedinIcon className='transition-all duration-200 ease-in-out hover:scale-125' /></a>
           <a href={siteMetadata.dribble} className='w-6 h-6'><DribbbleIcon className='transition-all duration-200 ease-in-out hover:scale-125' /></a>
