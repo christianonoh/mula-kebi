2cace19f12c4be9901511c70d2263b413fd544e5
Christian Onoh <chibyk5000@gmail.com>
2023-12-09 12:38:36 +0100

    Update styles for dark mode in AboutPage component

--- app/(about)/about/page.jsx
+++ app/(about)/about/page.jsx
@@ -23,10 +23,10 @@ const AboutPage = () => {
           />
         </div>
         <div className="items-center justify-center w-full px-5 py-10 lg:px-16 md:w-1/2">
-          <h2 className="mb-4 text-4xl font-semibold text-center sm:text-5xl text-dark lg:text-5xl">
+          <h2 className="mb-4 md:mb-8 text-4xl font-semibold text-center sm:text-5xl text-dark dark:text-light lg:text-6xl">
             Dream Big, Work Hard, Achieve More!
           </h2>
-          <p className="font-medium capitalize font-inter text-dark">
+          <p className="font-medium capitalize font-inter text-dark dark:text-light text-center md:text-left">
             This Mantra Drives My Work As A Passionate Freelancer. I Blend
             Innovative Technology With Timeless Design For Captivating Digital
             Experiences. Inspired By Nature And Literature, I'm A Perpetual
@@ -37,13 +37,13 @@ const AboutPage = () => {
       </section>
       
       <section className="max-w-5xl mx-5 mt-20 md:mx-10 lg:mx-auto">
-        <p className="mb-8 text-2xl font-semibold text-accent sm:text-4xl">
+        <p className="mb-8 text-2xl font-semibold text-accent dark:text-accentDark sm:text-4xl">
           I'm comfortable in...
         </p>
-        <ul className="flex flex-wrap justify-center gap-6 text-dark">
+        <ul className="flex flex-wrap justify-center gap-6 text-dark dark:text-light">
           {techStack.map((tech) => (
             <li
-              className="px-8 py-4 text-2xl font-semibold capitalize transition-all duration-200 ease-in-out border-2 border-solid rounded border-dark hover:scale-105"
+              className="px-6 md:px-8 py-2 md:py-4 text-lg sm:text-xl md:text-2xl font-semibold capitalize transition-all duration-200 ease-in-out border-2 border-solid rounded border-dark dark:border-light hover:scale-105"
               key={tech}
             >
               {tech}
@@ -53,7 +53,7 @@ const AboutPage = () => {
       </section>
       
       <section className="px-5 pt-8 mt-16 font-semibold border-t-2 border-solid border-dark md:px-10">
-        <div className="max-w-5xl mx-auto text-2xl text-center text-dark">
+        <div className="max-w-5xl mx-auto text-2xl text-center text-dark dark:text-light">
           {"Have a project in mind? Reach out to me 📞 from "}
           <Link href="/contact" className="underline cursor-pointer">
             here
