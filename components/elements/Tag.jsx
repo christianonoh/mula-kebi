db6d684138dba9bf996dc500994e5011b9b50e8d
Christian Onoh <chibyk5000@gmail.com>
2023-12-09 12:43:00 +0100

    Update Tag component styles for dark mode

--- components/elements/Tag.jsx
+++ components/elements/Tag.jsx
@@ -5,8 +5,8 @@ import Link from "next/link"
 const Tag = ({ link='#', title, ...props }) => {
   return (
     <Link href={link}
-      className={cx('inline-block px-10 py-3 font-semibold capitalize border rounded-full border-light text-light bg-dark/50',
-      'hover:scale-105 transition-all duration-200 ease-in-out border-2',
+      className={cx('inline-block px-6 py-2 sm:px-10 sm:py-3 font-semibold capitalize border rounded-full border-light text-light bg-dark/50',
+      'hover:scale-105 transition-all duration-200 ease-in-out border-2 text-sm sm:text-base',
       props.className )}>
       {title}
     </Link>
