63919e36500a781bd2898168a0cc668f0faecf87
Christian Onoh <chibyk5000@gmail.com>
2023-12-10 10:10:31 +0100

    Update compiler settings to remove console statements

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { withContentlayer } = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: { removeConsole: false },
+  compiler: { removeConsole: true },
   swcMinify: true,
 };
 
