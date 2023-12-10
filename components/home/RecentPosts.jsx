5f29092c05929e81f5a2ef5b1d8daedf032db6d9
Christian Onoh <chibyk5000@gmail.com>
2023-12-10 10:55:06 +0100

    Fix link typo in RecentPosts component

--- components/home/RecentPosts.jsx
+++ components/home/RecentPosts.jsx
@@ -11,14 +11,13 @@ const RecentPosts = () => {
   
   return (
     <section
-      // className={cx(styles.xMargins2x, styles.flexCenter, styles.topMargins, 'flex-col')}
       className='flex flex-col items-center justify-center w-full px-5 mt-16 sm:mt-24 md:mt-32 sm:px-10 md:px-24 sxl:px-32'
     >
       <div className="flex items-center justify-between w-full ">
         <h2 className="w-full text-2xl font-bold capitalize md:text-4xl dark:text-light">
           recent posts
         </h2>
-        <Link href='/catergories/all' className="px-4 py-2 text-sm font-semibold capitalize rounded-full sm:text-base md:text-lg whitespace-nowrap hover:text-dark text-accent bg-accent/10 dark:hover:text-light dark:text-accentDark dark:bg-accentDark/10">
+        <Link href='/categories/all' className="px-4 py-2 text-sm font-semibold capitalize rounded-full sm:text-base md:text-lg whitespace-nowrap hover:text-dark text-accent bg-accent/10 dark:hover:text-light dark:text-accentDark dark:bg-accentDark/10">
           view all
         </Link>
       </div>
