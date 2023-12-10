c2359256e5d6e9f685d9487161dd5403288763cd
Christian Onoh <chibyk5000@gmail.com>
2023-12-10 10:00:51 +0100

    Add scroll prop to Tag component

--- components/elements/Tag.jsx
+++ components/elements/Tag.jsx
@@ -2,12 +2,12 @@ import { cx } from "@/utils"
 import Link from "next/link"
 
 
-const Tag = ({ link='#', title, ...props }) => {
+const Tag = ({ link='#', title, scroll=true, ...props }) => {
   return (
     <Link href={link}
       className={cx('inline-block px-6 py-2 sm:px-10 sm:py-3 font-semibold capitalize border rounded-full border-light text-light bg-dark/50',
       'hover:scale-105 transition-all duration-200 ease-in-out border-2 text-sm sm:text-base',
-      props.className )}>
+      props.className )} scroll={scroll}>
       {title}
     </Link>
   )
